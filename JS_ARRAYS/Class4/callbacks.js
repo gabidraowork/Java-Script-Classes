dc7514d23731087f952c94d861df2b8047948808
Gabriel Caceres Teixeira <caceresgabrielwork@gmail.com>
2026-07-02 15:06:38 -0300

    git gi

---
+++ JS_ARRAYS/Class4/callbacks.js
@@ -0,0 +1,15 @@
+const names = ["Evaldo", "Mari", "Camis"];
+
+// names.forEach(function (name) {
+//     console.log(name);
+// });
+
+// names.forEach((name) => {
+//     console.log(name);
+// });
+
+function printName(name){        
+    console.log(name);
+}
+
+names.forEach(printName);
---
+++ JS_ARRAYS/Class4/map-names.js
@@ -0,0 +1,5 @@
+const names = ["ana Julia", "Caio vinicius" ,"BIA silva"]
+
+const namesUpperCase = names.map((name) => name.toUpperCase());
+
+console.log(namesUpperCase)
